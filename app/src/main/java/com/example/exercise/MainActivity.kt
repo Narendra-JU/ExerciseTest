1cf62c1409555d240098755d471a94d066efd2ce
Narendra Nath Chatterjee <narendra.chatterjee@tatadigital.com>
2021-03-03 13:24:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exercise/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.exercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        plus.setOnClickListener {
+
+        }
+
+
+    }
+}
